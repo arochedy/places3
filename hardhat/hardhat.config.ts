151c0a5e1f347144a0e992ef6259d4c8402f8a56
Adrien Rochedy <adrien.rochedy@gmail.com>
2023-04-09 18:21:10 +0200

    remove comments

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -4,7 +4,7 @@ import "solidity-coverage";
 import "hardhat-gas-reporter";
 
 require("dotenv").config();
-const { MNEMONIC, PROJECT_ID, PRIVATE_KEY, INFURA_ID } = process.env;
+const {  PROJECT_ID, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
@@ -16,28 +16,10 @@ const config: HardhatUserConfig = {
     coinmarketcap: "b88d1579-a2d9-46c5-8ffa-9ba45f41f774",
   },
   networks: {
-    // sepolia: {
-    //   url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-    //   accounts: [SEPOLIA_PRIVATE_KEY],
-    // },
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${PROJECT_ID}`,
-      // `https://polygon-mumbai.infura.io/v3/${INFURA_ID}`,
-      accounts: [
-        PRIVATE_KEY ??
-          "b0864106aa78591b301febb4f32ef7695c61b683cdbcde3973c18cd40ebb4a34",
-      ], //fake private key for testing lolca & cit
+      accounts: [PRIVATE_KEY],
       chainId: 80001,
-
-      // provider: () =>
-      //   new HDWalletProvider(
-      //     MNEMONIC,
-      //     `https://polygon-mumbai.g.alchemy.com/v2/${PROJECT_ID}`
-      //   ),
-      // network_id: 80001, // Goerli's id
-      // confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
-      // timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
   },
 };
--- hardhat/scripts/batman.ts
+++ hardhat/scripts/batman.ts
@@ -1,8 +1,5 @@
 import { ethers } from "hardhat";
 
-import fs from "fs-extra";
-import path from "path";
-
 async function dessinerBatman(xBase: number, yBase: number, places: any) {
   let color = 1;
   // Dessiner la première ligne du logo de Batman
@@ -105,54 +102,12 @@ async function dessinerBatman(xBase: number, yBase: number, places: any) {
   await places.votePixel(xBase + 13, yBase + 10, color);
 }
 
-// async function dessinerBatmanLogo(places: any) {
-//   // Coordonnées x, y des pixels pour le logo de Batman simplifié
-//   //ligne 1 du lgo :
-//   const line1 = [
-//     [0, 0],
-//     [1, 0],
-//     [2, 0],
-//     [3, 0],
-//     [4, 0],
-//     [5, 0],
-//     [6, 0],
-//     [7, 0],
-//     [8, 0],
-//   ];
-//   const line2 = [
-//     [1, 1],
-//     [2, 1],
-//     [3, 1],
-//     [4, 1],
-//     [5, 1],
-//     [6, 1],
-//     [7, 1],
-//     [8, 1],
-//   ];
-//   const line1 = [];
-
-//   // Couleur du logo
-//   const couleur = 0;
-
-//   // Boucle pour colorier les pixels
-//   for (let i = 0; i < pixels.length; i++) {
-//     const x = pixels[i][0];
-//     const y = pixels[i][1];
-//     await places.votePixel(x, y, couleur);
-//   }
-// }
-
-// Appeler la fonction pour dessiner le logo de Batman
-
 async function main() {
   let placesAddress = "0xC762E91BD28579575ef758e4cB9F1eA471De99Cc";
   const places = await ethers.getContractAt("Places", placesAddress);
 
   dessinerBatman(40, 0, places);
 }
-
-//  We recommend this pattern to be able to use async/await everywhere
-//  and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
